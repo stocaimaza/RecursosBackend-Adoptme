d51a57d033bea5b1360ec7f215f8ea06e8c76772
stocaimaza <stocaimaza@hotmail.com>
2024-05-29 10:23:26 -0300

    clase 45

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
+import cors from "cors";
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const connection = mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
